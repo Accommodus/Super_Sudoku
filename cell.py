7a869093315c276ee7ed5d9cb908c71cc6153345
benjamin-gold <149019369+benjamin-gold@users.noreply.github.com>
2023-12-04 12:43:26 -0500

    Update cell.py
    without draw method

--- cell.py
+++ cell.py
@@ -1,1 +1,12 @@
-c
+class Cell:
+    def __init__(self, value, row, col, screen):
+        self.value = value
+        self.row = row
+        self.col = col
+        self.screen = screen
+
+    def set_cell_value(self, value):
+        self.value = value
+
+    def draw(self):
+        pass
