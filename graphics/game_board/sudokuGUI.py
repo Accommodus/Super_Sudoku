aa818816b4da78b201602dbbca58b8dc413a1cb4
Cannon <cannonteaguewhitney8@gmail.com>
2023-12-05 14:14:38 -0500

    more space

--- graphics/game_board/sudokuGUI.py
+++ graphics/game_board/sudokuGUI.py
@@ -12,14 +12,14 @@ class GameBoard:
 
         pygame.init()
         self.screen_width = default_screen_width
-        self.screen_height = default_screen_height + 70
+        self.screen_height = default_screen_height
         pygame.display.set_caption(caption)
         self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
 
         self.font = pygame.font.Font(None, CHIP_FONT)
 
         HORIZONTAL_RATIO = self.screen_width // cell_number
-        VERTICAL_RATIO = self.screen_height // cell_number
+        VERTICAL_RATIO = (self.screen_height - 150) // cell_number
 
         CELL_WIDTH = HORIZONTAL_RATIO - cell_boarder_thickness
         CELL_HEIGHT = VERTICAL_RATIO - cell_boarder_thickness
